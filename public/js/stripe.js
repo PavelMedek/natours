2f62bf7d89137279d0e8c9a2890803988faece52
PavelMedek <medek41@gmail.com>
2022-08-21 09:31:52 +0200

    initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe(
+  'pk_test_51LYsFfBlaylmmm8cym70PtOeWDWjOM7jjaYWoTgwrEVvm1m0cWwPAcMK9E4t2y71EyEr4I613UyZnrqRYa8Dlpfe009pH7B3p5'
+);
+export const bookTour = async tourId => {
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
+
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
